5d670c5313e932d55456f55b1e2217c3c15c20b2
Jacob Sinding <Jacob_Sinding@hotmail.com>
2021-10-11 07:09:19 +0200

    added create and getcities

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ import jokeFacade from "./jokeFacade"
 
 document.getElementById("all-content").style.display = "block"
 
-
-document.getElementById("all-content").style.display = "block"
-
 //show all persons
 function fetchAllPersons() {
   fetch('https://abefisk.dk/ca/api/ca/all_persons')
@@ -35,11 +32,11 @@ let url;
 function getByPhone() {
   url = 'https://abefisk.dk/ca/api/ca/person_by_number/'+ document.getElementById("phone_number").value;
   fetch(url)
-    .then(res => res.json())
+    .then(handleHttpErrors)
     .then(data => 
     {
       const allRows = data.map(p => getPersonTableRow(p))
-      document.getElementById("person_by_phone_body").innerHTML = allRows.join("")
+      document.getElementById("person_by_phone").innerHTML = allRows.join("")
     });
 }
 
@@ -47,11 +44,11 @@ function getByPhone() {
 function getByHobby() {
   url = 'https://abefisk.dk/ca/api/ca/person_by_hobby/'+ document.getElementById("hobby").value;
   fetch(url)
-    .then(res => res.json())
+    .then(handleHttpErrors)
     .then(data => 
     {
       const allRows = data.map(p => getPersonTableRow(p))
-      document.getElementById("person_by_hobby_body").innerHTML = allRows.join("")
+      document.getElementById("person_by_hobby").innerHTML = allRows.join("")
     });
 }
 
@@ -59,11 +56,11 @@ function getByHobby() {
 function getByZip() {
   url = 'https://abefisk.dk/ca/api/ca/person_by_city/'+ document.getElementById("zip_code").value;
   fetch(url)
-    .then(res => res.json())
+    .then(handleHttpErrors)
     .then(data => 
     {
       const allRows = data.map(p => getPersonTableRow(p))
-      document.getElementById("person_by_zip_body").innerHTML = allRows.join("")
+      document.getElementById("person_by_zip").innerHTML = allRows.join("")
     });
 }
 
@@ -71,14 +68,53 @@ function getByZip() {
 function getPersonTableRow(p)
 {
   return `<tr>
-    <td>${p.id}</td>
-    <td>${p.email}</td>
-    <td>${p.first_name}</td>
-    <td>${p.last_name}</td>
-    <td>${p.phones}</td>
+    <th>${p.id}</th>
+    <th>${p.email}</th>
+    <th>${p.first_name}</th>
+    <th>${p.last_name}</th>
+    <th>${p.phones}</th>
 
     </tr>`
 }
+
+function createPerson() {
+  const data = {
+    email: document.getElementById("createEmail").value,
+    first_name: document.getElementById("createFirstName").value,
+    last_name: document.getElementById("createLastName").value
+  }
+
+  fetch(`https://abefisk.dk/ca/api/ca/create_person/`, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data)
+  });
+  alert("test")
+}
+
+function getAllCities() {
+  fetch(`https://abefisk.dk/ca/api/ca/get_all_zipcodes/`)
+  .then (handleHttpErrors)
+  .then (data => {
+    const allRows = data.map(p => getCities(p));
+    document.getElementById("get_cities_body").innerHTML = allRows.join("")
+  })
+}
+
+function getCities(data) {
+  return `<tr>
+  <th scope="row">${data.zipCode}</th>
+  <th>${data.city}</th>
+</tr>`
+}
+
 /* 
   Add your JavaScript for all exercises Below or in separate js-files, which you must the import above
 */
@@ -88,9 +124,11 @@ function hideAllShowOne(idToShow)
 {
   document.getElementById("about_html").style = "display:none"
   document.getElementById("person_html").style = "display:none"
+  document.getElementById("create_person_html").style = "display:none"
   document.getElementById("get_person_by_phone_html").style = "display:none"
   document.getElementById("get_person_by_hobby_html").style = "display:none"
   document.getElementById("get_person_by_zip_html").style = "display:none"
+  document.getElementById("get_cities_html").style = "display:none"
   document.getElementById("address_html").style = "display:none"
   document.getElementById(idToShow).style = "display:block"
 }
@@ -101,13 +139,37 @@ function menuItemClicked(evt)
   switch (id)
   {
     case "person": hideAllShowOne("person_html"); break
+    case "create_person": hideAllShowOne("create_person_html"); break
     case "get_person_by_phone": hideAllShowOne("get_person_by_phone_html"); break
     case "get_person_by_hobby": hideAllShowOne("get_person_by_hobby_html"); break
     case "get_person_by_zip": hideAllShowOne("get_person_by_zip_html"); break
-    case "address": hideAllShowOne("address_html"); break
+    case "get_cities": hideAllShowOne("get_cities_html");  getAllCities(); break
     default: hideAllShowOne("about_html"); break
   }
   evt.preventDefault();
+}
+
+
+function handleHttpErrors(res)
+{
+  if (!res.ok)
+  {
+    return Promise.reject({ status: res.status, fullError: res.json() })
+  }
+  return res.json();
+}
+
+function errorHandling(err)
+{
+  console.log(err)
+  if (err.status)
+  {
+    err.fullError.then(e => console.log(e.message))
+  }
+  else
+  {
+    console.log("Network error")
+  }
 }
 
 function submitAction(evt) {
@@ -120,8 +182,10 @@ function submitAction(evt) {
     getByHobby();
     break;
     case "zip_submit_button":
-      alert("test")
     getByZip();
+    break;
+    case "create_person_button":
+    createPerson();
     break;
     default: hideAllShowOne("about_html"); break
   }
@@ -131,6 +195,7 @@ document.getElementById("menu").onclick = menuItemClicked;
 document.getElementById("phone_number_submit_button").onclick = submitAction;
 document.getElementById("hobby_submit_button").onclick = submitAction;
 document.getElementById("zip_submit_button").onclick = submitAction;
+document.getElementById("create_person_button").onclick = submitAction;
 hideAllShowOne("about_html");
 
 
